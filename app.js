fb83dd3aa7e0e0376b0ae650d1d4999a8e0df005
Susheel_Pendyala <pendyalasusheel@gmail.com>
2023-11-08 16:22:58 -0600

    done upto screenshot 4 and 5

--- app.js
+++ app.js
@@ -3,12 +3,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Costume = require("./models/costume");
+require('dotenv').config();
+const { connection } = require('mongoose');
+mongoose = require('mongoose');
+const connectionString = process.env.MONGO_CON
+
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB: ', err));
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
+async function recreateDB() {
+  // Delete everything
+  await Costume.deleteMany();
+  let instance1 = new
+    Costume({
+      costume_type: "ghost", size: 'large',
+      cost: 15.4
+    });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var animalRouter = require('./routes/animal');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
+var ResourceRouter = require('./routes/resource');
 
 var app = express();
 
@@ -27,14 +64,15 @@ app.use('/users', usersRouter);
 app.use('/animal', animalRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
+app.use('/resource', ResourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
